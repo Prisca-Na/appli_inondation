827e1d984a2fa817461c93b7e3dbcd396e7c196c
Prisca NANA <priscanana00@gmail.com>
2025-06-27 12:59:45 +0000

    Mise à jour du code texte.py et des dépendances dans requirements.txt

--- texte.py
+++ texte.py
@@ -77,13 +77,10 @@ with col_inputs:
 
 
     secteurs_list = sorted(gdf_sectors['Secteur'].unique())
-    secteur_options = ["Ouagadougou_ville"] + [str(s) for s in secteurs_list]
+    secteur_options = ["Ouagadougou_ville"] + ["Secteur :"] + [str(s) for s in secteurs_list]
 
-    st.markdown("**Sélection des secteurs :**")
-    selection = st.multiselect("", options=secteur_options, default=[])
-    st.markdown("<span style='font-weight:bold;'>Secteur :</span>", unsafe_allow_html=True)
+    selection = st.multiselect("Sélectionnez des secteurs :", options=secteur_options, default=[])
 
-    # Validation de la sélection
     if not selection:
         st.warning("Veuillez sélectionner au moins un secteur.")
         st.stop()
